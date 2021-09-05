08b6614b996e12f951637451ca19809abb96eb09
Dimas Abimanyu <dmsabmny@gmail.com>
2021-09-05 10:22:27 +0700

    add repo link to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="container">
-      <a href="/" alt="Dimas Abimanyu">
+      <a href="https://github.com/dimasabimanyy/react-portfolio" alt="Dimas Abimanyu" target="_blank" rel="noreferrer">
         &copy; Copyright 2021. Designed and Built by Dimas Abimanyu.
       </a>
     </footer>
